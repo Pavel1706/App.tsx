96ecce7749ea6cd47bca88988265e740c548c8d0
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-10-07 00:53:46 +0300

    add useCallback

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,16 +1,22 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 import {Box, Button, FormControl, IconButton, TextField} from '@material-ui/core';
 import {AddBox} from '@material-ui/icons';
+import { useCallback } from 'react';
 
 type AddItemFormPropsType = {
     addItem: (title: string) => void
 }
 
-export function AddItemForm(props: AddItemFormPropsType) {
-
+export const AddItemForm= React.memo(function (props: AddItemFormPropsType) {
+    console.log('AddItemForm is called')
     let [title, setTitle] = useState("")
     let [error, setError] = useState<string | null>(null)
 
+
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
     const addItem = () => {
         if (title.trim() !== "") {
             props.addItem(title);
@@ -20,12 +26,10 @@ export function AddItemForm(props: AddItemFormPropsType) {
         }
     }
 
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setTitle(e.currentTarget.value)
-    }
-
     const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        setError(null);
+        if(error) {
+            setError(null);
+        }
         if (e.charCode === 13) {
             addItem();
         }
@@ -44,4 +48,4 @@ export function AddItemForm(props: AddItemFormPropsType) {
             <AddBox />
         </IconButton>
     </div>
-}
+})
